8849ec56660f3912d15363c659d16cd57e63f7f8
RAJAN  KUMAR JHA <148378859+Rajanjha12@users.noreply.github.com>
2025-06-16 02:53:42 +0530

    Update Signup.jsx

--- Frontend/BookStore/src/Component/Signup.jsx
+++ Frontend/BookStore/src/Component/Signup.jsx
@@ -20,7 +20,7 @@ const Signup = () => {
       email:data.email,
       password:data.password
      };
-      await axios.post("http://localhost:4001/user/signup",userInfo)
+      await axios.post("https://kitab-ghar-backend.onrender.com",userInfo)
       //promise is used to handle the response from the server
       .then((res)=>
      {
